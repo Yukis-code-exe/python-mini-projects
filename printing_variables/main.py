4da280556e55a5e62e3c5c93d3a28cbb0745b32b
Logen Straun <Logen.straun@hotmail.co.uk>
2025-12-26 18:29:05 +0000

    learn: printing and variables

---
+++ printing_variables/main.py
@@ -0,0 +1,10 @@
+#Printing and Variables: Start with a super simple script that just prints out a message and uses 
+#variables to store a couple of pieces of information. This will get you comfortable with the absolute basics.
+
+#age = 22
+
+print("How old are you?" )
+age = input()
+#print(age)
+
+print("Oh, you are", age, "years old")
